e0a68e8fd49844f983afb7a2ceee3ad3270f47a6
Jithin R <jithin.r@experionglobal.com>
2022-11-22 14:12:10 +0530

    jithi commit courseList  class

--- src/main/java/com/experion/mainProject/lms/entity/CourseList.java
+++ src/main/java/com/experion/mainProject/lms/entity/CourseList.java
@@ -1,4 +1,33 @@
 package com.experion.mainProject.lms.entity;
 
-public class CourseList {
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="courseList")
+@Data
+public class CourseList
+{
+    @Id
+    @Column(name = "courseId")
+    private  String courseId;
+    @ManyToOne
+    @JoinColumn(name="categoryId", nullable = false)
+    private CourseCategory category;
+
+    @Column(name = "courseName")
+    private String courseName;
+
+
+    @Column(name = "courseDescreption")
+    private String courseDescreption;
+
+    @Column(name = "CourseUrl")
+    private String CourseUrl;
+    
+
 }
