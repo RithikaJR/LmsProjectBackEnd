e00c44a5eb1d903215b1e6b1fa25e7f3f85683bd
rithika.jr <rithika.jr@experionglobal.com>
2022-11-24 16:55:55 +0530

    search api generated

--- src/main/java/com/experion/mainProject/lms/dao/CourseListRepository.java
+++ src/main/java/com/experion/mainProject/lms/dao/CourseListRepository.java
@@ -2,11 +2,15 @@ package com.experion.mainProject.lms.dao;
 
 
 import com.experion.mainProject.lms.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "course", path="courses")
 @CrossOrigin(origins="*")
 public interface CourseListRepository extends JpaRepository<Course,String> {
+    Page<Course> findAllBycourseName(@Param("name")String courseName,Pageable pageable);
 }
