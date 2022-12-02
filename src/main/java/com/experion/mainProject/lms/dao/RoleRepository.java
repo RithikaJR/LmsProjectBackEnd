9fb7811fd1e74dc2b90380170ebbdf6d24330f20
vaishnav.cc <vaishnav.cc@experionglobal.com>
2022-12-02 15:50:39 +0530

    vaishnav's commit on 02/12/22

---
+++ src/main/java/com/experion/mainProject/lms/dao/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.experion.mainProject.lms.dao;
+
+import com.experion.mainProject.lms.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@RepositoryRestResource(collectionResourceRel = "role", path="roles")
+@CrossOrigin(origins="*")
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
