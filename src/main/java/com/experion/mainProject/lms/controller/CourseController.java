b4562e40fcfc1994ed1d5e9d1406250c22f3cc3e
rithika.jr <rithika.jr@experionglobal.com>
2022-12-19 12:46:15 +0530

    forget password reject mail merged

--- src/main/java/com/experion/mainProject/lms/controller/CourseController.java
+++ src/main/java/com/experion/mainProject/lms/controller/CourseController.java
@@ -1,15 +1,20 @@
 package com.experion.mainProject.lms.controller;
 
 import com.experion.mainProject.lms.dto.AddCourse;
+import com.experion.mainProject.lms.dto.AddModule;
+import com.experion.mainProject.lms.dto.RejectMailRequest;
 import com.experion.mainProject.lms.service.CourseServices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
 
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/add-course")
+//@RequestMapping("/api/add-course")
 public class CourseController
 {
     @Autowired
@@ -19,10 +24,24 @@ public class CourseController
 //        this.addCourseServices = addCourseServices;
 //    }
 
-    @PostMapping("/add")
+    @PostMapping("*/course/add-course")
     public String callAddCourse(@RequestBody AddCourse addCourse) {
         String  response=addCourseServices.addCourse(addCourse);
         return response;
 
     }
+
+    @PostMapping("*/module/add-module")
+    @PreAuthorize("hasRole('Super Admin')")
+    public String callAddModule(@RequestBody AddModule addModule) {
+        String response = addCourseServices.addModule(addModule);
+        return response;
+
+    }
+
+    @PostMapping("*/course/reject-mail-notify")
+    public void courseRejectMail(@RequestBody RejectMailRequest rejectMailRequest) throws MessagingException {
+        addCourseServices.sendRejectMail(rejectMailRequest);
+    }
+
 }
